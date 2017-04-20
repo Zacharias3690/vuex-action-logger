a8e266eb26de9a58b0a9e700aa58fff782b7489e
Zach <zbarnes@redventures.com>
2017-04-20 11:44:49 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+import * as Vuex from 'vuex';
+
+export default {
+  install() {
+    let dispatchCalls = [];
+    let temp = Vuex.Store.prototype.dispatch;
+
+    Vuex.Store.prototype.dispatch = function actionWatcher() {
+      dispatchCalls.push(arguments);
+      return temp.apply(this, arguments);
+    };
+
+    // Log on an interval to avoid blocking
+    setInterval(() => {
+      while(dispatchCalls.length) {
+        console.log(dispatchCalls.shift());
+      }
+    }, 1000);
+  }
+}
